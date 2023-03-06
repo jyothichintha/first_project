1d5a0bdf3555ab8e018f592cced3a47a83e2f6b9
jyothichintha <jyothichintha1999.com>
2023-03-06 22:25:06 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def jyo(request):
+    return HttpResponse('jyothi lerning python fullstoct development') 
